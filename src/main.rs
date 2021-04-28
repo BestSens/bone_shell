14cd28b879c8036441547c71341c4bfe82969db1
Jan Schöppach <jan.schoeppach@bestsens.de>
2021-04-28 17:57:32 +0200

    Implement shell mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::*;
 use structopt::StructOpt;
 use bone_api::Bone;
 
@@ -16,7 +17,7 @@ pub struct Opt {
 	#[structopt(long)]
 	password: Option<String>,
 
-	command: String
+	command: Option<String>
 }
 
 fn main() -> std::io::Result<()> {
@@ -24,7 +25,6 @@ fn main() -> std::io::Result<()> {
 
 	let ip = opt.connect;
 	let port = opt.port;
-	let command = opt.command;
 
 	let mut bone1 = Bone::new(&ip, &port);
 	bone1.connect();
@@ -41,10 +41,52 @@ fn main() -> std::io::Result<()> {
 		}
 	}
 
-	let parsed = bone1.send_command(&command);
-	let pretty_response = json::stringify_pretty(parsed, 4);
+	let data = bone1.send_command(&json::object!{"command" => "serial_number"}.dump());
 
-	println!("{}", pretty_response);
+	if let Some(command) = &opt.command {
+		// command mode
+		let parsed = bone1.send_command(&command);
+		let pretty_response = json::stringify_pretty(parsed, 4);
+
+		println!("{}", pretty_response);
+	} else {
+		// shell mode
+		let alias = &data["payload"]["alias"];
+		let cnt_str;
+
+		if !alias.is_string() {
+			let serial_number = &data["payload"]["serial_number"];
+
+			if !serial_number.is_string() {
+				cnt_str = String::from("");
+			} else {
+				cnt_str = serial_number.to_string();
+			}
+		} else {
+			cnt_str = alias.to_string();
+		}
+
+		loop {
+			print!("{} > ", cnt_str);
+			stdout().flush().unwrap();
+
+			let mut command = String::new();
+			stdin().read_line(&mut command).unwrap();
+
+			let result = json::parse(&command);
+			match result {
+				Err(msg) => eprintln!("invalid input: {}", msg),
+				Ok(command) => {
+					let command = json::stringify(command);
+
+					let parsed = bone1.send_command(&command);
+					let pretty_response = json::stringify_pretty(parsed, 4);
+
+					println!("{}", pretty_response);
+				}
+			}
+		}
+	}
 
 	Ok(())
 }
